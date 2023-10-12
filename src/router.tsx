a6358534386502d01f157a6e0831fbc595330896
filoscoder <sondaniel.88@gmail.com>
2023-10-12 15:24:21 +0200

    feat: remove player path

--- src/router.tsx
+++ src/router.tsx
@@ -8,13 +8,11 @@ import {
 import App from "./App";
 
 const Home = lazy(() => import("./pages/home"));
-const Player = lazy(() => import("./pages/player"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/player" element={<Player />}></Route>
     </Route>,
   ),
 );
