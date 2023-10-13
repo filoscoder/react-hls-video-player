29e08cf14bcbf69817bdd96b5ff110e9ff6c03aa
filoscoder <sondaniel.88@gmail.com>
2023-10-13 14:10:05 +0200

    feat: removed link from video_sources

--- src/const/links.ts
+++ src/const/links.ts
@@ -1,8 +1,7 @@
-export const BE_PRO_POSTER_LINK =
-  "https://i.ytimg.com/vi/lJjRF5k--60/maxresdefault.jpg";
+export const DEFAULT_POSTER_LINK =
+  "https://img.myloview.com/posters/glitch-vhs-effect-white-horizontal-lines-on-dark-backdrop-retro-video-texture-with-white-noise-no-signal-template-old-analog-camera-concept-vector-illustration-400-201535801.jpg";
 
 export const VIDEO_SOURCES = [
-  "https://s.bepro11.com/videos-eu/7a449dcafd/2020-10-22T06:26:42_8c3e076c65db258d/playlist.m3u8",
   "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
   "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
   "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
